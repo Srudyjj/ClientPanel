0ad2288c5f8e511ba1474f9a63cdba52fdea7356
Turbinist <srudyjj@gmail.com>
2018-10-21 14:49:41 +0300

    Add AddClient

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
-import AppNavBar from './components/layout/AppNavBar';
-import DashBoard from './components/layout/DashBoard';
+import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
+import AddClient from './components/clients/AddClient';
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all"
 
 class App extends Component {
   render() {
@@ -11,10 +14,11 @@ class App extends Component {
       <Provider store={store} >
         <Router>
           <div className="">
-            <AppNavBar />
+            <AppNavbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={DashBoard} />
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/client/add" component={AddClient} />
               </Switch>
             </div>
 
--- src/store.js
+++ src/store.js
@@ -5,21 +5,23 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBC4b3JxGkMT5o7NHqe2ETrVlcsbbstGjI",
-  authDomain: "clientpanel-d8d12.firebaseapp.com",
-  databaseURL: "https://clientpanel-d8d12.firebaseio.com",
-  projectId: "clientpanel-d8d12",
-  storageBucket: "clientpanel-d8d12.appspot.com",
-  messagingSenderId: "952485196948"
+  apiKey: 'AIzaSyBC4b3JxGkMT5o7NHqe2ETrVlcsbbstGjI',
+  authDomain: 'clientpanel-d8d12.firebaseapp.com',
+  databaseURL: 'https://clientpanel-d8d12.firebaseio.com',
+  projectId: 'clientpanel-d8d12',
+  storageBucket: 'clientpanel-d8d12.appspot.com',
+  messagingSenderId: '952485196948'
 };
 // firebase config
 const rrfConfig = {
-  userProfile: "users",
+  userProfile: 'users',
   useFirestoreForProfile: true
 };
 
 firebase.initializeApp(firebaseConfig);
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+firestore.settings(settings);
 
 const createStoreWithFirebase = compose(
   reactReduxFirebase(firebase, rrfConfig),
@@ -33,11 +35,13 @@ const rootReduser = combineReducers({
 
 const initialState = {};
 
-const store = createStoreWithFirebase(rootReduser, initialState, compose(
-  reactReduxFirebase(firebase),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const store = createStoreWithFirebase(
+  rootReduser,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
-
-
